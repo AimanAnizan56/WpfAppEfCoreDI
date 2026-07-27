b6f4021748022f393a8acca7d78bd3fd9bf35e01
AimanAnizan56 <anizanaimann1@gmail.com>
2026-07-27 23:05:04 +0800

    Perform migration to the database for CreatedAt and ModifiedAt

--- WpfAppEfCoreDI/Migrations/20260727150045_AddTimestampsToEntities.Designer.cs
+++ WpfAppEfCoreDI/Migrations/20260727150045_AddTimestampsToEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EntityFrameworkCore.Jet.Metadata;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WpfAppEfCoreDI.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using WpfAppEfCoreDI.Infrastructure.Data;
 namespace WpfAppEfCoreDI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260727150045_AddTimestampsToEntities")]
+    partial class AddTimestampsToEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WpfAppEfCoreDI/Migrations/20260727150045_AddTimestampsToEntities.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WpfAppEfCoreDI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimestampsToEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Products",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Products",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Products");
+        }
+    }
+}
