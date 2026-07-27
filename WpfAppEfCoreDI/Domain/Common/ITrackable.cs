e7044bc27406fdbea462c6be718bb0379ec35f5d
AimanAnizan56 <anizanaimann1@gmail.com>
2026-07-27 23:03:25 +0800

    Create interface to track CreatedAt and ModifiedAt

---
+++ WpfAppEfCoreDI/Domain/Common/ITrackable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfAppEfCoreDI.Domain.Common
+{
+    public interface ITrackable
+    {
+        DateTime CreatedAt { get; set; }
+        DateTime ModifiedAt { get; set; }
+    }
+}
