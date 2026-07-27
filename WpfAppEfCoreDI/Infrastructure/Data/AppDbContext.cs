df2812e83d4190854e284e3d858a367989c070b6
AimanAnizan56 <anizanaimann1@gmail.com>
2026-07-27 23:04:35 +0800

    Apply CreatedAt and ModifiedAt in database context before saving into database

--- WpfAppEfCoreDI/Infrastructure/Data/AppDbContext.cs
+++ WpfAppEfCoreDI/Infrastructure/Data/AppDbContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WpfAppEfCoreDI.Domain.Common;
 using WpfAppEfCoreDI.Domain.Entities;
 
 namespace WpfAppEfCoreDI.Infrastructure.Data
@@ -18,6 +19,36 @@ namespace WpfAppEfCoreDI.Infrastructure.Data
             modelBuilder.Entity<Product>()
                 .Property(p => p.ID)
                 .ValueGeneratedOnAdd();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            OnBeforeSaving();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        public override int SaveChanges()
+        {
+            OnBeforeSaving();
+            return base.SaveChanges();
+        }
+
+        private void OnBeforeSaving()
+        {
+            var entries = ChangeTracker.Entries()
+                .Where(e => e.Entity is ITrackable &&
+                (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+            var utcNow = DateTime.UtcNow;
+
+            foreach(var entry in entries)
+            {
+                var trackable = entry.Entity as ITrackable;
+                if(entry.State == EntityState.Added)
+                {
+                    trackable!.CreatedAt = utcNow;
+                }
+                trackable!.ModifiedAt = utcNow;
+            }
         }
     }
 }
